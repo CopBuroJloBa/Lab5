f83059fe304e10ad8d98db3055002794c787519c
Cupofjuiceh <klimbaxoxo@gmail.com>
2016-12-05 22:31:52 +0700

    First commit, added main.c

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <math.h>
+#include <conio.h>
+#include <stdio.h>
+
+int main(){
+	float a, b, c;
+	float x1=0, x2=0;
+	printf("ax^2 + bx + c = 0\n");
+	printf("Enter a, b, c: ");
+	scanf("%f %f %f",&a, &b, &c);
+	getch();
+	return 0;
+}
